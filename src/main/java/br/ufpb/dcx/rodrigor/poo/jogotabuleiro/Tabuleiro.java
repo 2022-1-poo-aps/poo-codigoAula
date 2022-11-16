fc5f0c0ba1742ba406e4c8a0e290a36eaab0e80b
Rodrigo Rebouças de Almeida <rodrigor@dcx.ufpb.br>
2022-11-16 08:38:07 -0300

    BUGFIX: erro ao mover peças nas bordas do tabuleiro

--- src/main/java/br/ufpb/dcx/rodrigor/poo/jogotabuleiro/Tabuleiro.java
+++ src/main/java/br/ufpb/dcx/rodrigor/poo/jogotabuleiro/Tabuleiro.java
@@ -78,28 +78,37 @@ public class Tabuleiro {
 
 
         // Checar movimento da peça do jogo de Damas:
-        List<MovimentoPossivel> possiveis = new LinkedList<>();
+        List<MovimentoPossivel> possiveisSimples = new LinkedList<>();
 
         //peças vizinhas
         //TODO: considerar a situação onde as peças não podem "voltar". Da forma como está implementado, a peça pode fazer um movimento "pra trás"
         //TODO: incluir a informação do "lado" do tabuleiro, onde a peça está localizada. Pensar: isso é o tabuleiro que informa? (refletir sobre isso)
-        possiveis.add((orig, dest) -> dest.x == orig.x-1 && dest.y == orig.y+1);
-        possiveis.add((orig, dest) -> dest.x == orig.x+1 && dest.y == orig.y+1);
-        possiveis.add((orig, dest) -> dest.x == orig.x-1 && dest.y == orig.y-1);
-        possiveis.add((orig, dest) -> dest.x == orig.x+1 && dest.y == orig.y-1);
+        possiveisSimples.add((orig, dest) -> dest.x == orig.x-1 && dest.y == orig.y+1);
+        possiveisSimples.add((orig, dest) -> dest.x == orig.x+1 && dest.y == orig.y+1);
+        possiveisSimples.add((orig, dest) -> dest.x == orig.x-1 && dest.y == orig.y-1);
+        possiveisSimples.add((orig, dest) -> dest.x == orig.x+1 && dest.y == orig.y-1);
+
+
+        if(possiveisSimples.stream().anyMatch(possivel -> possivel.movimentoValido(origem, destino))){
+            movimentarPeca(origem, destino);
+            return;
+        }
 
         //capturando peças (1 nível)
+        List<MovimentoPossivel> possiveisCaptura = new LinkedList<>();
+
         //TODO: melhorar o uso do optional usando ifPresent()
         //Lógica aqui: o getCasa retorna um Optional "null" caso a coordenada passada esteja fora dos limites do tabuleiro.
         //isso é necessário para que possamos testar situações onde a coordenada de origem seja na borda do tabuleiro.
         //Por isso verificamos primeiro se a cada "existe" getCasa(...).isPresent()
-        possiveis.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x - 1, orig.y - 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x - 2 && dest.y == orig.y - 2));
-        possiveis.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x - 1, orig.y + 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x - 2 && dest.y == orig.y + 2));
-        possiveis.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x + 1, orig.y - 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x + 2 && dest.y == orig.y - 2));
-        possiveis.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x + 1, orig.y + 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x + 2 && dest.y == orig.y + 2));
+        possiveisCaptura.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x - 1, orig.y - 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x - 2 && dest.y == orig.y - 2));
+        possiveisCaptura.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x - 1, orig.y + 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x - 2 && dest.y == orig.y + 2));
+        possiveisCaptura.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x + 1, orig.y - 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x + 2 && dest.y == orig.y - 2));
+        possiveisCaptura.add((orig, dest) -> getCasa(orig.x - 1, orig.y - 1).isPresent() && !getCasa(orig.x + 1, orig.y + 1).get().temPecaMesmaCor(pecaOrigem) && (dest.x == orig.x + 2 && dest.y == orig.y + 2));
 
-        if(possiveis.stream().anyMatch(possivel -> possivel.movimentoValido(origem, destino))){
+        if(possiveisSimples.stream().anyMatch(possivel -> possivel.movimentoValido(origem, destino))){
             movimentarPeca(origem, destino);
+            //TODO: implementar captura da peça
         }else {
             throw new MovimentoInvalidoException("Movimento inválido");
         }
